2c86bcf4090ca832cc276293988f6f69392e21b9
zia-hussain <syedziashahgillani76@gmail.com>
2024-05-21 19:26:39 +0500

    First Main Background Comp Added !

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+function Background() {
+  return (
+    <>
+      <div className="fixed z-[2] w-full h-screen ">
+        <div className="absolute top-[5%] w-full py-10 flex justify-center text-xl text-zinc-600 font-semibold">
+          Documents
+        </div>
+        <h1 className=" absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-[12vw] leading-none tracking-tighter font-semibold text-zinc-900">
+          Docs.
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Background;
