69f713bd4466426a0c3e3fd4c391b67954d80460
zia-hussain <syedziashahgillani76@gmail.com>
2024-05-21 19:26:53 +0500

    Cards Main Comp Added !

---
+++ src/components/ForGround.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+function ForGround() {
+  return (
+    <div>
+      <div className="fixed z-[3] w-full h-full ">
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default ForGround;
